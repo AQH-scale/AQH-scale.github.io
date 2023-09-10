1f3a6595ec66fd55c5ce4b47340b9b6ecef3cf0a
Angela Yang <angela@MacBook-Air-2.local>
2023-09-09 20:34:56 -0700

    added more variables + fixed scrolling

--- templates/script.js
+++ templates/script.js
@@ -1,21 +1,17 @@
 let back1 = document.getElementById('back1');
 let back2 = document.getElementById('back2');
 let back3 = document.getElementById('back3');
-
-const initHeight = document.body.scrollHeight;
+let elem = document.getElementById('textBox');
+let box = elem.getBoundingClientRect();
 
 window.addEventListener('scroll', () => {
-  // find a value it shouldnt be a constant
-  // also maybe hide scroll bar idk
-  console.log(initHeight)
-  if (window.scrollY < initHeight) {
-    let value = window.scrollY;
+    if (window.scrollY + box.height < box.bottom) {
+        let value = window.scrollY;
 
-    back1.style.top = value * 1.8 + 'px';
-    back2.style.top = value * 1.1 + 'px';
-    back3.style.top = value * 1.3 + 'px';
-  }
-  
+        back1.style.top = value * 1.8 + 'px';
+        back2.style.top = value * 1.1 + 'px';
+        back3.style.top = value * 1.3 + 'px';
+    }
 });
 
 var activeLink = null;
@@ -36,3 +32,28 @@ inputs.forEach((input) => {
   input.addEventListener("focus", focusFunc);
   input.addEventListener("blur", blurFunc);
 });
+
+function setVariables() {
+    var aqi = 100;
+    var condition = "Hazardous";
+    var colorClass = "maroon";
+    if (aqi >= 0 && aqi <= 50) {
+        condition = "Good";
+        colorClass = "green";
+    } else if (aqi <= 100) {
+        condition = "Moderate";
+        colorClass = "yellow";
+    } else if (aqi <= 150) {
+        condition = "Unhealthy for sensitive groups";
+        colorClass = "orange";
+    } else if (aqi <= 200) {
+        condition = "Unhealthy";
+        colorClass = "red";
+    } else if (aqi <= 300) {
+        condition = "Very unhealthy";
+        colorClass = "purple";
+    }
+    document.getElementById("airCondition").innerHTML = condition;
+    document.getElementById("aqiNum").innerHTML = aqi;
+    document.getElementById("color").innerHTML = colorClass;
+}
